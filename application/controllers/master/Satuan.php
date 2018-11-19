7fcc3f8b8551c24eb931530eaa3a34548e27361d
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2018-11-19 21:13:06 +0700

    push satuan

--- application/controllers/master/Satuan.php
+++ application/controllers/master/Satuan.php
@@ -11,4 +11,25 @@ class Satuan extends CI_Controller {
 		$this->load->view("master/satuan");
 	}
 
+	function ajaxTable()
+	{
+
+		$list = $this->M_satuan->tampil();
+    	$data = array();
+        $no = $_POST['start'];
+    	foreach ($list as $key) {
+            $no++;
+    		$row = array();
+            $row[] = $no;
+			$data[] = $row;
+            
+    	}
+    	$output = array("draw" => $_POST['draw'],
+                        "recordsTotal" => $this->M_satuan->count_all(),
+                        "recordsFiltered" => $this->M_satuan->count_filtered(),
+                        "data" => $data);
+
+    	echo json_encode($output);
+	}
+
 }
